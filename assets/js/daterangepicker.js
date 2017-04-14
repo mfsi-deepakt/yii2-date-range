7e20713704e377f64a20cebc643ce11d627794db
mfsi-deepakt <deepakt@mindfiresolutions.com>
2017-04-14 12:07:10 +0530

    Remove calendar icon from the template view

--- assets/js/daterangepicker.js
+++ assets/js/daterangepicker.js
@@ -141,7 +141,6 @@
                 '<div class="calendar right">' +
                     '<div class="daterangepicker_input">' +
                       '<input class="input-mini" type="text" name="daterangepicker_end" value="" />' +
-                      '<i class="fa fa-calendar glyphicon glyphicon-calendar"></i>' +
                       '<div class="calendar-time">' +
                         '<div></div>' +
                         '<i class="fa fa-clock-o glyphicon glyphicon-time"></i>' +
